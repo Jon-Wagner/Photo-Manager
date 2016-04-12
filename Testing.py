04f57e7f4b12a39b676cb03381f285d4965c5e92
MisterIbis <wagner.jon+github@gmail.com>
2016-04-12 00:26:27 -0400

    SQLite management
    database management mostly implemented, but messy

--- Testing.py
+++ Testing.py
@@ -1,28 +1,59 @@
 import unittest
-from Photo import Photo
+from TestBed import TestBed
 
 '''
 TODO
 -: test passes
 +: code is no longer ugly
 
-store a photo to the db
-load a photo from the db
-have a photo object
+- store a photo to the db
+- load a photo from the db
 display a photo
-add tags to a photo
+- add column for a tag to the db
+- add tags to a photo
+sanitize inputs for every method
 '''
 
 class TestPhotoManager(unittest.TestCase):
-    def test_create_photo(self):
-        photo = Photo('filepath')
-        self.assertEquals(photo.filepath, 'filepath')
-        
-    def test_insert_photo(self):
-        self.assertTrue(True)
+    photo_manager = None
+    test_string_1 = 'test.png'
+    test_string_2 = 'other_test.png'
+    test_default_location = 'nowhere'
+    test_alternate_location = 'somewhere'
+
+    def setUp(self):
+        self.photo_manager = TestBed(':memory:')
+        self.photo_manager.create_table("File_Name TEXT")
+        self.photo_manager.insert_photo(self.test_string_1)
+
+    def tearDown(self):
+        self.photo_manager.kill()
 
     def test_select_photo(self):
-        self.assertTrue(True)
-        
-if __name__  == 'main':
+        self.photo_manager.insert_photo(self.test_string_2)
+        tmp = self.photo_manager.select_photo('File_Name',self.test_string_1)
+        self.assertEqual(tmp[0][0], self.test_string_1)
+        self.assertNotEqual(tmp[0][0], self.test_string_2)
+
+    def test_insert_column(self):
+        self.photo_manager.insert_photo(self.test_string_1)
+        self.photo_manager.insert_column("Geographic_Location TEXT", self.test_default_location)
+        tmp = self.photo_manager.select_photo('File_Name',self.test_string_1)
+        self.assertEqual(tmp[0][1], self.test_default_location)
+
+    # need to do something about determining how to handle arbitrary data types in columns instead of assuming everything is a string
+    def test_selecting_on_second_col(self):
+        self.photo_manager.insert_column("Geographic_Location TEXT", self.test_default_location)
+        self.photo_manager.insert_photo(self.test_string_2 + "', '" + self.test_alternate_location)
+        tmp = self.photo_manager.select_photo('Geographic_Location', self.test_default_location)
+        self.assertEqual(tmp[0][1], self.test_default_location)
+        self.assertEqual(tmp[0][0], self.test_string_1)
+
+    def test_changing_a_value(self):
+        self.photo_manager.insert_column("Geographic_Location TEXT", self.test_default_location)
+        self.photo_manager.change_value('File_Name', self.test_string_1, 'Geographic_Location', self.test_alternate_location)
+        tmp = self.photo_manager.select_photo('Geographic_Location', self.test_alternate_location)
+        self.assertEqual(tmp[0][1], self.test_alternate_location)
+
+if __name__  == '__main__':
     unittest.main()
--- testbed.py
+++ testbed.py
@@ -16,18 +16,34 @@ All using of data should be seperate from any actual IO. Goal is to be database 
 '''
 class TestBed:
     db_connection = None
-    
-    def __init__(self, filepath):
-        self.db_connection = lite.connect(filepath)
-    
-        
-    '''
-    def 
-        with db_connection:  
-            db_cursor = db_connection.cursor()    
-            db_cursor.execute('SELECT SQLITE_VERSION()')
-            
-            data = db_cursor.fetchone()
-            
-            print("SQLite version: %s" % data)                
-    '''
+    db_cursor = None
+
+    def __init__(self, db_path):
+        self.db_connection = lite.connect(db_path)
+        with self.db_connection:
+            self.db_cursor = self.db_connection.cursor()
+
+    def create_table(self,  typed_columns):
+        self.db_cursor.execute("CREATE TABLE Photos (" + typed_columns + ")")
+
+    def insert_column(self, typed_column, default_value):
+        self.db_cursor.execute("ALTER TABLE Photos ADD COLUMN " + typed_column + " DEFAULT '" + default_value + "'")
+
+    def insert_photo(self, row_values):
+        self.db_cursor.execute("INSERT INTO Photos VALUES ('" + row_values + "')")
+
+    # this may be vulnerable to SQL injection, column and value should be sanitized
+    def select_photo(self, typed_column, value):
+        self.db_cursor.execute("SELECT * FROM Photos WHERE " + typed_column + " = '" + value + "'")
+        return self.db_cursor.fetchall()
+
+    def change_value(self, match_column, match_value, change_column, change_value):
+        self.db_cursor.execute("UPDATE Photos SET " + change_column + " = '" + change_value + "' WHERE " + match_column + " = '" + match_value + "'")
+
+    def sanitize_string(self, unsanitized_string):
+        sanitized_string = ''
+
+        return sanitized_string
+
+    def kill(self):
+        self.db_cursor.close()
